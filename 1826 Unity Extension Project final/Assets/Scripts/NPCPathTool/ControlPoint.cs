3c36718d9378d1a5bbacd37e4c286df3d1a4a5bd
Tim <81233357+tim859@users.noreply.github.com>
2026-01-22 15:44:11 +0000

    updated comments

--- 1826 Unity Extension Project final/Assets/Scripts/NPCPathTool/ControlPoint.cs
+++ 1826 Unity Extension Project final/Assets/Scripts/NPCPathTool/ControlPoint.cs
@@ -1,27 +1,21 @@
-/* 
-Disclaimer: The creation of some parts of this class were inspired by the second answer to this stack overflow post
-(https://stackoverflow.com/questions/50788120/how-to-make-individual-anchor-points-of-bezier-continuous-or-non-continuous)
-*/
-
 using System;
 using UnityEngine;
 
 [Serializable]
-public class ControlPoint // not inheriting from monobehaviour so that Unity won't complain when this class is directly instantiated
+public class ControlPoint
 {
     [SerializeField] Vector3 position;
     [SerializeField] Vector3 backTangent;
     [SerializeField] Vector3 frontTangent;
 
-    public ControlPoint(Vector3 initialPosition) // constructor for ControlPoint class, control points always require a position point,
-                                                 // a front tangent point and a back tangent point
+    // control points always require a position point, a front tangent point and a back tangent point
+    public ControlPoint(Vector3 initialPosition)
     {
         position = initialPosition;
         frontTangent = Vector3.one;
         backTangent = -Vector3.one;
     }
-
-    // the majority of this classes methods are simply getters and setters for the different properties
+    
     public Vector3 GetPosition()
     {
         return position;
